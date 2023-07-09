e3bdb9009a73ff33515433dbbc2b55e05f411284
AurobindoGupta <aurogpt10@gmail.com>
2023-07-09 20:52:16 +0530

    improvement

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 (function () {
-  let str = "aabbskhslfglljjjjw";
-  let flag;
+  let str = "abbslkhslfglajjjjw";
+  let flag = {};
   for (let i = 0; i < str.length; i++) {
-    let count = 0;
-
-    if (str[i] !== flag) {
-      for (let k = 0; k < str.length; k++) {
-        if (str[i] === str[k]) {
-          count += 1;
-          if (count > 1) {
-            flag = str[i];
-           
-          }
-        }
-      }
-
-      console.log(str[i], "=", count);
+    let count = 0; 
+    if (flag[str[i]]) {
+      flag[str[i]] = flag[str[i]] + 1;
+    } else {
+      flag[str[i]] = 1;
     }
+    
+
   }
+  console.log({flag});
 })();
